fd2181d497ccdb64fff714519ffb5b0e9f036e66
Hector Gonzalez <gorozco.hector@gmail.com>
2022-09-20 18:46:47 -0400

    Ships can get hit through gameboard

--- src/model.js
+++ src/model.js
@@ -1,78 +1,107 @@
-const Ship = function(length){
-    // Array full with ones, 
-    const shipSquares = new Array(length).fill(1)
-    let sunk = false;
+const Ship = function (length) {
+  // Array full with ones,
+  const shipSquares = new Array(length).fill(1);
+  let sunk = false;
 
-    const isSunk = function (){
-        sunk = shipSquares.every(hit => hit === 0);
-        return sunk
+  const isSunk = function () {
+    sunk = shipSquares.every((hit) => hit === 0);
+    return sunk;
+  };
+
+  const getLength = function () {
+    return length;
+  };
+
+  const getHit = function (index) {
+    // Only if index is not out of bounds
+    if (index >= length || index < 0) {
+      return shipSquares;
     }
+    shipSquares[index] = 0;
+    // if every square is hit, sunk returns true
+    console.log(shipSquares)
+    return shipSquares;
+  };
 
-    const getLength = function(){
-        return length
+  const getSquares = function(){
+    return shipSquares
+  }
+
+  return { isSunk, getLength, getHit, getSquares};
+};
+
+const Gameboard = function () {
+  // board is a 10x10 2d array of zeros
+  const board = Array(10)
+    .fill()
+    .map(() => Array(10).fill(0));
+  // Keeps track of which ship is where.
+  const positionGraph = Array(10)
+    .fill()
+    .map(() => [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}]);
+
+  const getCurrentBoard = function () {
+    return board;
+  };
+
+
+  const isBlocked = function (length, rowCoord, columnCoord, direction) {
+    let subArray;
+    if (direction === "horizontal") {
+      // get row
+      subArray = board[rowCoord].slice(columnCoord, columnCoord + length);
+    } else {
+      // get only relevant rows
+      subArray = board.filter(
+        (row, i) => i >= rowCoord && i < rowCoord + length
+      );
+      // get only relevant columns
+      subArray = subArray.map((row) =>
+        row.filter((column, i) => i === columnCoord)
+      );
+      subArray = subArray.flat();
     }
+    const isAnyOccupied = subArray.some((square) => square === 1);
+    const isOutOfBounds = subArray.length !== length;
+    return isAnyOccupied || isOutOfBounds;
+  };
 
-    const getHit = function(index){
-        // Only if index is not out of bounds
-        if(index >= length || index < 0){
-            return shipSquares
+  const placeShip = function (ship, rowCoord, columnCoord, direction) {
+    const length = ship.getLength();
+    // Change board only if space is not occupied or it doesn't go out of bounds
+    if (!isBlocked(length, rowCoord, columnCoord, direction)) {
+      if (direction === "horizontal") {
+        for (let i = 0; i < length; i++) {
+          board[rowCoord][i + columnCoord] = 1;
+          // Update list that keeps track of indices corresponding in ships
+          const positionObject = positionGraph[rowCoord][i + columnCoord]
+          console.log(positionObject)
+          positionObject.ship = ship;
+          positionObject.shipSquare = i;
         }
-        shipSquares[index] = 0;
-        // if every square is hit, sunk returns true
-        return shipSquares
-    }
-
-    return { isSunk, getLength, getHit}
-}
-
-
-const Gameboard = function(){
-    // board is a 10x10 2d array of zeros
-    const board = Array(10).fill().map(() => Array(10).fill(0));
-    // Keeps track of which ship is where.
-    const positionGraph = new Array(10).fill(new Array(10)) 
-
-    const getCurrentBoard = function(){
-        return board
-    }
-
-    const isPlaceable = function(length, rowCoord, columnCoord, direction){
-        let subArray
-        if(direction === 'horizontal'){
-            // get row
-            subArray = board[rowCoord].slice(columnCoord, columnCoord+length);
-        } else {
-            // get only relevant rows
-            subArray = board.filter((row, i) => i >= rowCoord && i < rowCoord + length )
-            // get only relevant columns
-            subArray = subArray.map(row => row.filter((column, i) => i === columnCoord))
-            subArray = subArray.flat()
+      } else {
+        // if direction is vertical
+        for (let i = 0; i < length; i++) {
+          board[rowCoord + i][columnCoord] = 1;
+          positionGraph[rowCoord + 1][columnCoord].ship = ship;
+          positionGraph[rowCoord + 1][columnCoord].shipSquare = i;
+          
         }
-        const isAnyOccupied = subArray.some(square => square === 1);
-        const isOutOfBounds = subArray.length !== length;
-        return isAnyOccupied || isOutOfBounds
+      }
     }
+  };
 
-    const placeShip = function(ship, rowCoord, columnCoord, direction){
-        const length = ship.getLength()
-        // Change board only if space is not occupied or it doesn't go out of bounds
-        if(!isPlaceable(length, rowCoord, columnCoord, direction)){
-            if(direction === 'horizontal'){
-            
-                for(let i=0; i < length; i++){
-                    board[rowCoord][i + columnCoord] = 1
-                }
-            } else {// if direction is vertical
-                for(let i=0; i < length; i++){
-                    board[rowCoord + i][columnCoord] = 1
-                } 
-            }
-        }
+
+  const receiveAttack = function(rowCoord, columnCoord){
+    // If square is ocuppied, send hit to corresponding ship
+    if(board[rowCoord][columnCoord] === 1){
+        // console.log(positionGraph)
+        const shipInfo = positionGraph[rowCoord][columnCoord];
+        return shipInfo.ship.getHit(shipInfo.shipSquare)
     }
+  }
 
-
-    return { getCurrentBoard, placeShip}
-}
-
+  return { getCurrentBoard, placeShip, receiveAttack};
+};
 
 export { Ship, Gameboard };
--- src/model.test.js
+++ src/model.test.js
@@ -1,131 +1,161 @@
-import { Ship , Gameboard} from './model'
+import { Ship, Gameboard } from "./model";
 
-test('Ship object returns false to sunk when created', () => {
-    const newShip = Ship(3);
-    expect(newShip.isSunk()).toBe(false)
-})
+test("Ship object returns false to sunk when created", () => {
+  const newShip = Ship(3);
+  expect(newShip.isSunk()).toBe(false);
+});
 
-test('Ship object outputs correct length', () => {
-    const newShip = Ship(5);
-    expect(newShip.getLength()).toBe(5)
-})
+test("Ship object outputs correct length", () => {
+  const newShip = Ship(5);
+  expect(newShip.getLength()).toBe(5);
+});
 
-test('Ship object capable of getting hit', () => {
-    const newShip = Ship(5);
-    
-    // Can't hit out of bounds
-    expect(newShip.getHit(-1)).toEqual([1, 1, 1, 1, 1])
+test("Ship object capable of getting hit", () => {
+  const newShip = Ship(5);
 
-    expect(newShip.getHit(1)).toEqual([1, 0, 1, 1, 1])
-    expect(newShip.getHit(0)).toEqual([0, 0, 1, 1, 1])
+  // Can't hit out of bounds
+  expect(newShip.getHit(-1)).toEqual([1, 1, 1, 1, 1]);
 
-    // Can't hit twice
-    expect(newShip.getHit(0)).toEqual([0, 0, 1, 1, 1])
-    
-})
+  expect(newShip.getHit(1)).toEqual([1, 0, 1, 1, 1]);
+  expect(newShip.getHit(0)).toEqual([0, 0, 1, 1, 1]);
 
-test('Ship object capable of being sunk', () => {
-    const newShip = Ship(1);
-    expect(newShip.isSunk()).toEqual(false)
-    newShip.getHit(0);
-    expect(newShip.isSunk()).toEqual(true)
-    
-})
+  // Can't hit twice
+  expect(newShip.getHit(0)).toEqual([0, 0, 1, 1, 1]);
+});
 
-test('Gameboard empty when created', () => {
-    expect(Gameboard().getCurrentBoard()).toEqual([
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    ])
-    
-})
+test("Ship object capable of being sunk", () => {
+  const newShip = Ship(1);
+  expect(newShip.isSunk()).toEqual(false);
+  newShip.getHit(0);
+  expect(newShip.isSunk()).toEqual(true);
+});
 
 
-test('Gameboard updated when placing ship', () => {
+
+test("Gameboard empty when created", () => {
+  expect(Gameboard().getCurrentBoard()).toEqual([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+});
+
+test("Gameboard updated when placing ship", () => {
+  const newShip = Ship(5);
+  const newGameboard = Gameboard();
+  // place a ship horizontally
+  newGameboard.placeShip(newShip, 0, 0, "horizontal");
+
+  expect(newGameboard.getCurrentBoard()).toEqual([
+    [1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+
+  // place a ship vertically
+  newGameboard.placeShip(Ship(3), 2, 3, "vertical");
+  expect(newGameboard.getCurrentBoard()).toEqual([
+    [1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+});
+
+test("Gameboard not updated when ship is placed out of bounds", () => {
+  const newShip = Ship(5);
+  const newGameboard = Gameboard();
+  newGameboard.placeShip(newShip, 0, 0, "horizontal");
+  newGameboard.placeShip(Ship(3), 2, 3, "vertical");
+
+  // Don't update if ship is to big for available space horizontally
+  newGameboard.placeShip(Ship(3), 2, 2, "horizontal");
+  expect(newGameboard.getCurrentBoard()).toEqual([
+    [1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+
+  newGameboard.placeShip(Ship(3), 5, 6, "horizontal");
+  // Don't update if ship is to big for available space vertically
+  newGameboard.placeShip(Ship(5), 4, 7, "vertical");
+  expect(newGameboard.getCurrentBoard()).toEqual([
+    [1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+
+  // Don't update if ship is to big for board bounds
+  newGameboard.placeShip(Ship(3), 9, 2, "vertical");
+  expect(newGameboard.getCurrentBoard()).toEqual([
+    [1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+});
+
+test("Gameboard receiveAttack hits a ship", () => {
     const newShip = Ship(5);
     const newGameboard = Gameboard();
     // place a ship horizontally
-    newGameboard.placeShip(newShip, 0, 0, 'horizontal')
-
+    newGameboard.placeShip(newShip, 1, 0, "horizontal");
+  
     expect(newGameboard.getCurrentBoard()).toEqual([
-        [1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    ])
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]);
 
-    // place a ship vertically
-    newGameboard.placeShip(Ship(3), 2, 3, 'vertical')
-    expect(newGameboard.getCurrentBoard()).toEqual([
-        [1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    ])
+    newGameboard.receiveAttack(1, 1)
 
-    // Don't update if ship is to big for available space horizontally
-    newGameboard.placeShip(Ship(3), 2, 2, 'horizontal')
-    expect(newGameboard.getCurrentBoard()).toEqual([
-        [1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    ])
-
-    newGameboard.placeShip(Ship(3), 5, 6, 'horizontal')
-    // Don't update if ship is to big for available space vertically
-    newGameboard.placeShip(Ship(5), 4, 7, 'vertical')
-    expect(newGameboard.getCurrentBoard()).toEqual([
-        [1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 1, 1, 1, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    ])
-
-    // Don't update if ship is to big for board bounds
-    newGameboard.placeShip(Ship(3), 9, 2, 'vertical')
-    expect(newGameboard.getCurrentBoard()).toEqual([
-        [1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 1, 1, 1, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    ])
-})
+    expect(newShip.getSquares()).toEqual([1, 0, 1, 1, 1])
+  
+  });
